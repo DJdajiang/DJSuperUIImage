e1cab893793dd0d89407ecd71aae05034af7f6cc
刘文江 <464280789@qq.com>
2016-11-15 09:10:04 +0800

    DJSuperUIImage
    对UIImage的一个类别，可以简单的生成你需要的圆角图片，高清图片。。

---
+++ UIImage+DJExtension.h
@@ -0,0 +1,44 @@
+//
+//  UIImage+DJExtension.h
+//  KCKC
+//
+//  Created by 刘文江 on 2016/11/1.
+//  Copyright © 2016年 刘文江. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (DJExtension)
+
+/**
+ *  生成一张高斯模糊的图片
+ *
+ *  @param image 原图
+ *  @param blur  模糊程度 (0~1)
+ *
+ *  @return 高斯模糊图片
+ */
++ (UIImage *)blurImage:(UIImage *)image blur:(CGFloat)blur;
+
+/**
+ *  根据颜色生成一张图片
+ *
+ *  @param color 颜色
+ *  @param size  图片大小
+ *
+ *  @return 图片
+ */
++ (UIImage *)imageWithColor:(UIColor *)color size:(CGSize)size;
+
+/**
+ *  生成圆角的图片
+ *
+ *  @param originImage 原始图片
+ *  @param borderColor 边框原色
+ *  @param borderWidth 边框宽度
+ *
+ *  @return 圆形图片
+ */
++ (UIImage *)circleImage:(UIImage *)originImage borderColor:(UIColor *)borderColor borderWidth:(CGFloat)borderWidth;
+
+@end
---
+++ UIImage+DJExtension.m
@@ -0,0 +1,152 @@
+//
+//  UIImage+DJExtension.m
+//  KCKC
+//
+//  Created by 刘文江 on 2016/11/1.
+//  Copyright © 2016年 刘文江. All rights reserved.
+//
+
+#import "UIImage+DJExtension.h"
+#import <Accelerate/Accelerate.h>
+
+@implementation UIImage (DJExtension)
+
+/**
+ *  生成一张高斯模糊的图片
+ *
+ *  @param image 原图
+ *  @param blur  模糊程度 (0~1)
+ *
+ *  @return 高斯模糊图片
+ */
++ (UIImage *)blurImage:(UIImage *)image blur:(CGFloat)blur
+{
+    // 模糊度越界
+    if (blur < 0.f || blur > 1.f) {
+        blur = 0.5f;
+    }
+    int boxSize = (int)(blur * 40);
+    boxSize = boxSize - (boxSize % 2) + 1;
+    CGImageRef img = image.CGImage;
+    vImage_Buffer inBuffer, outBuffer;
+    vImage_Error error;
+    void *pixelBuffer;
+    //从CGImage中获取数据
+    CGDataProviderRef inProvider = CGImageGetDataProvider(img);
+    CFDataRef inBitmapData = CGDataProviderCopyData(inProvider);
+    //设置从CGImage获取对象的属性
+    inBuffer.width = CGImageGetWidth(img);
+    inBuffer.height = CGImageGetHeight(img);
+    inBuffer.rowBytes = CGImageGetBytesPerRow(img);
+    
+    inBuffer.data = (void*)CFDataGetBytePtr(inBitmapData);
+    
+    pixelBuffer = malloc(CGImageGetBytesPerRow(img) *
+                         CGImageGetHeight(img));
+    
+    if(pixelBuffer == NULL)
+        NSLog(@"No pixelbuffer");
+    
+    outBuffer.data = pixelBuffer;
+    outBuffer.width = CGImageGetWidth(img);
+    outBuffer.height = CGImageGetHeight(img);
+    outBuffer.rowBytes = CGImageGetBytesPerRow(img);
+    
+    error = vImageBoxConvolve_ARGB8888(&inBuffer, &outBuffer, NULL, 0, 0, boxSize, boxSize, NULL, kvImageEdgeExtend);
+    
+    if (error) {
+        NSLog(@"error from convolution %ld", error);
+    }
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef ctx = CGBitmapContextCreate(
+                                             outBuffer.data,
+                                             outBuffer.width,
+                                             outBuffer.height,
+                                             8,
+                                             outBuffer.rowBytes,
+                                             colorSpace,
+                                             kCGImageAlphaNoneSkipLast);
+    CGImageRef imageRef = CGBitmapContextCreateImage (ctx);
+    UIImage *returnImage = [UIImage imageWithCGImage:imageRef];
+    
+    CGContextRelease(ctx);
+    CGColorSpaceRelease(colorSpace);
+    
+    free(pixelBuffer);
+    CFRelease(inBitmapData);
+    
+    CGColorSpaceRelease(colorSpace);
+    CGImageRelease(imageRef);
+    
+    return returnImage;
+
+}
+
+/**
+ *  根据颜色生成一张图片
+ *
+ *  @param color 颜色
+ *  @param size  图片大小
+ *
+ *  @return 图片
+ */
++ (UIImage *)imageWithColor:(UIColor *)color size:(CGSize)size
+{
+    if (color) {
+        CGRect rect = CGRectMake(0, 0, size.width, size.height);
+        UIGraphicsBeginImageContext(rect.size);
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        CGContextSetFillColorWithColor(context,color.CGColor);
+        CGContextFillRect(context, rect);
+        UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        return img;
+    }
+    return nil;
+}
+
+/**
+ *  生成圆角的图片
+ *
+ *  @param originImage 原始图片
+ *  @param borderColor 边框原色
+ *  @param borderWidth 边框宽度
+ *
+ *  @return 圆形图片
+ */
++ (UIImage *)circleImage:(UIImage *)originImage borderColor:(UIColor *)borderColor borderWidth:(CGFloat)borderWidth
+{
+    //设置边框宽度
+    CGFloat imageWH = originImage.size.width;
+    
+    //计算外圆的尺寸
+    CGFloat ovalWH = imageWH + 2 * borderWidth;
+    
+    //开启上下文
+    UIGraphicsBeginImageContextWithOptions(originImage.size, NO, 0);
+    
+    //画一个大的圆形
+    UIBezierPath *path = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, 0, ovalWH, ovalWH)];
+    
+    [borderColor set];
+    
+    [path fill];
+    
+    //设置裁剪区域
+    UIBezierPath *clipPath = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(borderWidth, borderWidth, imageWH, imageWH)];
+    [clipPath addClip];
+    
+    //绘制图片
+    [originImage drawAtPoint:CGPointMake(borderWidth, borderWidth)];
+    
+    //从上下文中获取图片
+    UIImage *resultImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    //关闭上下文
+    UIGraphicsEndImageContext();
+    return resultImage;
+
+}
+@end
